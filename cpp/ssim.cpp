428af39d77f124e7b8169ab43285814af9d9390a
shivam <sshivam21@iitk.ac.in>
2023-10-09 21:36:47 +0530

    updates

--- cpp/ssim.cpp
+++ cpp/ssim.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<torch/torch.h>
 
+
 torch::Tensor fspecial_gauss(int size, float sigma){
     torch::Tensor coords = torch::arange(size).to(torch::kFloat32);
     coords -= size/2;
@@ -10,19 +11,29 @@ torch::Tensor fspecial_gauss(int size, float sigma){
     return g.unsqueeze(0).unsqueeze(0);
 }
 
-// torch::Tensor gaussian_filter(torch::Tensor inp, torch::Tensor win){
-//     auto shape = inp.sizes();
-//     int dimsum = 0;
-//     for(auto dim : shape) dimsum += dim;
+torch::Tensor gaussian_filter_2d(torch::Tensor inp, torch::Tensor win){
+    auto in_shape = inp.sizes();
+    auto win_shape = win.sizes();
+    
+    int c = in_shape[1];
+    torch::Tensor out = inp.detach().clone();
+    
+    using namespace torch::nn::functional;
+    out = conv2d(out, win.transpose(2, -1), Conv2dFuncOptions().stride(1).padding(0).groups(c));
+    out = conv2d(out, win.transpose(3, -1), Conv2dFuncOptions().stride(1).padding(0).groups(c));
+    return out;
+}
 
-//     torch::nn::Conv2d conv2;
-//     torch::nn::Conv3d conv3;
-
-//     int c = shape[1];
-//     torch::Tensor out = inp.detach().clone();
-
-// }
+void test_gaussian(){
+    std::string device = "cpu";
+    if(torch::cuda::is_available()) device = "cuda";
+    torch::Tensor X = torch::rand({32, 3, 128, 128}).to(device);
+    torch::Tensor win = fspecial_gauss(11, 1.5).to(device);
+    win = win.repeat({3, 1, 1, 1});
+    std::cout << gaussian_filter_2d(X, win);
+}
 
 int main(){
-    std::cout << fspecial_gauss(3, 0.5);
+    // std::cout << fspecial_gauss(3, 0.5);
+    test_gaussian();
 }
